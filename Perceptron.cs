a517e088cfd1d455a2201c7c04ffc0886f06b89d
JXB1001 <bentley.silas@gmail.com>
2019-04-25 16:34:03 +0100

    Initial Commit

---
+++ Perceptron.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Neuroley
+{
+    class Perceptron
+    {
+        private float[] weights;
+        private float bias;
+        private float learningRate = 0.1f;
+
+        public Perceptron(int numberOfInputs, float learningRate)
+        {
+            this.learningRate = learningRate;
+            Initialse(numberOfInputs);
+        }
+
+        public Perceptron(int numberOfInputs)
+        {
+            Initialse(numberOfInputs);
+        }
+
+        private void Initialse(int numberOfInputs)
+        {
+            Random random = new Random();
+            weights = new float[numberOfInputs];
+            bias = 1;
+            for (int i = 0; i < weights.Length; i++)
+                weights[i] = (float)random.NextDouble() * 2 - 1;
+        }
+
+        public float Guess(float[] input)
+        {
+            float output = 0;
+            for(int i = 0; i < input.Length; i++)
+            {
+                for(int j = 1; j < input.Length; j++)
+                {
+                    output += weights[i] * input[j];
+                }
+            }
+            return output + bias;
+        }
+
+        public void Train(float[] input, float goal)
+        {
+            float guess = Guess(input);
+            float error = goal - guess;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                for (int j = 1; j < input.Length; j++)
+                {
+                    weights[i] +=  input[j]*error*learningRate;
+                }
+            }
+        }
+
+        public float Evaluate(float[] input, float goal)
+        {
+            float guess = Guess(input);
+            float error = goal - guess;
+            return error;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Neuroley
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Perceptron brain = new Perceptron(2);
+            float guess = brain.Guess(new float[]{2,1});
+            Console.Out.WriteLine(guess.ToString());
+        }
+    }
+}
